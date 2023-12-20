63b2b1c62e2fc7fa26f2530f5231b790de5a9884
Sona210 <153424510+Sona210@users.noreply.github.com>
2023-12-20 18:37:48 +0400

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,31 @@
+# 1
+ls /etc | grep '[0-9]'
+
+# 2
+echo -e "Hello\nWorld\nTestlo\nHillo\nOtherlo\n" >test_file.txt
+grep -E '\b\w{3}lo$' test_file.txt
+
+# 3
+grep -h '[A-Z]' список_каталогов*.txt:  
+grep -h '[-AZ]' список_каталогов*.txt:  
+grep -h '^[A-Z]' список_каталогов*.txt: 
+grep -h '[^A-Z]' список_каталогов*.txt: 
+
+# 4
+grep -E '^(bz|zip)' dirlist-bin.txt dirlist-sbin.txt
+
+# 5
+grep -Eh '^(bz|gz|zip)' список_каталогов*.txt 
+grep -Eh '^bz|gz|zip' список_каталогов*.txt   
+
+# 6
+echo "test@test.com" | grep -E '^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$'
+
+# 7
+echo "192.168.0.1" | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$'
+
+# 9
+echo "01/02/1970" | sed -E 's~([0-9]+)/([0-9]+)/([0-9]+)~\3-\2-\1~'
+
+# 10
+echo "0xx 12-34-56" | sed -E 's~^(0xx) ([0-9]{2})-([0-9])([0-9])-([0-9]{2})$~(\1) \2\3-\4\5~'
